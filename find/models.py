d2749adf722700eb66203aed340f695f863bb6fb
mishalareekkan <mishalareekkan@gmail.com>
2024-06-20 10:40:17 +0530

    sdfgv

--- find/models.py
+++ find/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-# class Vehicle_type(models.Model):
-#     name = models.CharField(max_length=250)
-#     register_no = models.CharField(max_length=100)
-#     owner = models.CharField(max_length=250)
-#     model = models.IntegerField()
-#     image = models.ImageField(upload_to="cars/")
-#     notes = models.TextField()
+class Vehicle_type(models.Model):
+    name = models.CharField(max_length=250)
+    register_no = models.CharField(max_length=100)
+    owner = models.CharField(max_length=250)
+    model = models.IntegerField()
+    image = models.ImageField(upload_to="cars/")
+    notes = models.TextField()
